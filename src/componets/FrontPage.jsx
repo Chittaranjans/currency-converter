08f460f02b96df5b7508d9e138eab943ef0fbf2e
Chittaranjans <chittaedu18@gmail.com>
2024-05-06 13:37:22 +0530

    progress on homepage _auth

--- src/componets/FrontPage.jsx
+++ src/componets/FrontPage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function FrontPage() {
   return (
-      <div className='text-white font-extrabolds'>frontPage
+    <div className= "bg-cover bg-center absolute inset-0 flex flex-col justify-center items-center  ">
+          <img src="/public/pexels-pixabay-164527.jpg" alt="cover" className='w-full absolute top-0 left-0 h-1/2 object-cover object-center border rounded-md      ' />
+          
+          <p className='text-white text-left bg-transparent mt-20 absolute inset-0 flex flex-col '>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi eos illum ipsa quam, quia accusamus. Quod sed excepturi in reiciendis tempora sit praesentium sapiente, voluptates, velit iure libero, ex quisquam?</p>
+    
+      <h1 className='text-white text-left bg-transparent mt-20 absolute inset-0 flex flex-col pt-2'>hiiii</h1>  
+      
       </div>
   )
 }
